a0ec118bb2377223e5e437659b266ff30e46f687
Aaron Sushinsky <aaronsushinsky@gmail.com>
2025-12-04 08:14:52 -0600

    Added triangle with movement

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import *
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -16,7 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
+        player.update(dt)
 if __name__ == "__main__":
     main()
